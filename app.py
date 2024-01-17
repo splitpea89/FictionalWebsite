aac9ce711d001b59a29be72a06d7e412d2a7ead3
splitpea89 <benashaheen@icloud.com>
2024-01-17 14:43:12 -0800

    confirmation page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -17,6 +17,21 @@ def products():
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+
+@app.route('/confirmation')
+def confirmation():
+    name = request.args.get('name')
+    email = request.args.get('email')
+    phone = request.args.get('phone')
+    address = request.args.get('address')
+    props = {
+        "name": name,
+        "email": email,
+        "phone": phone,
+        "address": address,
+    }
+    return render_template("confirmation.html", data=props)
+
 
 
 if __name__ == '__main__':
